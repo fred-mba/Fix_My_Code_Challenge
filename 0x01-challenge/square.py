b3de946ce5bf59ad4248068eb87804946c171fe9
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-04-17 01:02:47 +0300

    My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-""" Perfect square square implementation"""
+"""
+An implementation of a perfect square
+"""
 
 
-class Square():
-    """ Class square implemetation """
+class square():
+    """Class square function"""
+
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Initialization of single and keyworded arguments """
+        """Initialize single and keyword arguments"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -16,18 +19,18 @@ class Square():
         """ Area of the square """
         return self.width * self.height
 
-    def permiter_of_my_square(self):
-        """ Perimeter of the square """
+    def perimeter_of_my_square(self):
+        """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ Printable representation """
+        """String representation"""
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """ Create a square object """
-    s = Square(width=12, height=9)
+
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.permiter_of_my_square())
+    print(s.perimeter_of_my_square())
